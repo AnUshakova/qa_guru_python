794ec4b6c0c1ddb8b07494039563ba87a12baa7b
anarielen <70214887+anarielen@users.noreply.github.com>
2022-09-04 12:02:12 +0300

    second HW

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -1,7 +1,22 @@
 from selene.support.shared import browser
 from selene import be, have
+import pytest
 
 
-browser.open('https://google.com')
-browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+@pytest.fixture()
+def open_browser():
+    browser.config.window_width = 1200
+    browser.config.window_height = 800
+
+
+def test_positiv_find(open_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene python').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+
+def test_negativ(open_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene python').press_enter()
+    browser.element('[id="search"]').should(have.no.text('yashaka/selene: питон жив'))
