41bbb3f71e5e03b74c0681804385c904f50a0f4c
AnUshakova <112799450+AnUshakova@users.noreply.github.com>
2022-10-02 18:49:14 +0300

    Update function.py
    Second version

--- function.py
+++ function.py
@@ -1,18 +1,17 @@
-def func_name(func, locals):
-    print(f'{func.__name__} {locals}')
-
-
 def open_browser(browser_name):
     func_name(open_browser, locals())
-
 
 def go_to_companyname_homepage(page_url):
     func_name(go_to_companyname_homepage, locals())
 
-
 def find_registration_button_on_login_page(page_url, button_text):
     func_name(find_registration_button_on_login_page, locals())
 
+def print_name_func(func_name, *args):
+    func_name = func_name.__name__.title().replace('_', ' ')
+    print(f'Name of function is {func_name}. Function arguments is ')
+    for arg in args:
+        print(arg)
 
 open_browser('Chrome')
 go_to_companyname_homepage('https://mail.ru/')
